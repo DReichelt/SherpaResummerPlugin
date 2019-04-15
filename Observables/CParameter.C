17d30ce7f02c9d0b9b67961041ce0a94923047a9
reichelt <danielreichelt@me.com>
2019-04-15 09:58:36 -0500

    Add C-Parameter as Observable.

---
+++ Observables/CParameter.C
@@ -0,0 +1,17 @@
+#include <algorithm>
+using std::max;
+#include "CParameter.H"
+
+
+using namespace RESUM;
+
+typedef CParameter_Template<double> CParameter;
+
+DECLARE_GETTER(CParameter,"CParameter",Observable_Base,Observable_Key);
+Observable_Base *ATOOLS::Getter<Observable_Base,Observable_Key,CParameter>::
+operator()(const Parameter_Type &args) const 
+{ return new CParameter(args); }
+void ATOOLS::Getter<Observable_Base,Observable_Key,CParameter>::
+PrintInfo(std::ostream &str,const size_t width) const
+{ str<<"Heavy Jet Mass"; }
+
