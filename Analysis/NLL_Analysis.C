b13a52b8a2ff87f98ae30550ee1e08995f0889da
reichelt <danielreichelt@me.com>
2020-01-22 15:20:51 +0100

    Adapt NLL analysis.

--- Analysis/NLL_Analysis.C
+++ Analysis/NLL_Analysis.C
@@ -2,6 +2,7 @@
 
 #include "Analysis/Observable_Base.H"
 #include "Main/Resum.H"
+#include "Analysis/ChannelAlgorithms/ChannelAlgorithm_Base.H"
 
 namespace RESUM {
 
@@ -18,23 +19,23 @@ namespace RESUM {
 
     Resum *p_resum;
 
-    size_t nObs = 0;
-    std::vector<std::string> ObsNames;
-    double N = 0;
-    double sumW = 0;
-    double sumW2 = 0;
-    ObsDist _h_NLL;
-    ObsDist _h_expLO;
-    ObsDist _h_expNLO;
-    ChDist _h_NLL_channels;
-    ChDist _h_expLO_channels;
-    ChDist _h_expNLO_channels;
+    size_t m_nObs = 0;
+    std::vector<std::string> m_ObsNames;
+    double m_Ncount = 0;
+    double m_sumW = 0;
+    double m_sumW2 = 0;
+    ObsDist m_NLL;
+    ObsDist m_expLO;
+    ObsDist m_expNLO;
+    ChDist m_NLL_channels;
+    ChDist m_expLO_channels;
+    ChDist m_expNLO_channels;
     std::vector<std::string> m_channels;
 
     void Output(const std::string& name, const CumDist& dist,
                 const std::vector<double>& xvals);
 
-    
+    std::vector<ChannelAlgorithm_Base::Ptr> m_channelAlgs;    
   public:
 
     NLL_Analysis(const ANALYSIS::Argument_Matrix &params);
@@ -77,11 +78,28 @@ NLL_Analysis::NLL_Analysis(const Argument_Matrix& params):
     THROW(fatal_error,"Resummer plugin not loaded");
   }
   p_resum->ResetObservables();
+  m_channelAlgs.clear();
   for(size_t i=1; i<params.size(); i++) {
+    if (params[i][0] == "ChAlg") {
+      m_channelAlgs.emplace_back(ChAlg_Getter::GetObject(params[i][1],
+                                                         {params[i][1],
+                                                          {&params[i][1],&params[i][params.size()-1]}}));
+      continue;
+    }
+    // to stay consistent with previous defaults
+    if(m_channelAlgs.size()==0 &&
+     reader.GetValue<std::string>("RESUM::SORT_CHANNELS","YES")!="NO") {
+      int nborn = p_resum->nLegs()-2;
+      m_channelAlgs.emplace_back(ChAlg_Getter::GetObject("KT2_ee",
+                                                         {"KT2_ee",
+                                                          {std::to_string(nborn),
+                                                           ""}}));
+    }
+
     if (params[i].size()<5) continue;
-    nObs++;
+    m_nObs++;
     const string& obsName = params[i][0];
-    ObsNames.push_back(obsName);
+    m_ObsNames.push_back(obsName);
     const double xmin = ATOOLS::ToType<double>(ip->Interprete(params[i][1]));
     const double xmax = ATOOLS::ToType<double>(ip->Interprete(params[i][2]));
     const int nbin = ATOOLS::ToType<int>(ip->Interprete(params[i][3]));
@@ -90,10 +108,11 @@ NLL_Analysis::NLL_Analysis(const Argument_Matrix& params):
     vector<double> xvals(dummy.Nbin()+1);
     xvals[0] = dummy.LowEdge(0);
     for(size_t i=0; i<dummy.Nbin(); i++) xvals[i] = dummy.HighEdge(i);
-    _h_NLL[obsName] = CumDist(xvals.size(),{0,0});
-    _h_expLO[obsName] = CumDist(xvals.size(),{0,0});
-    _h_expNLO[obsName] = CumDist(xvals.size(),{0,0});    
+    m_NLL[obsName] = CumDist(xvals.size(),{0,0});
+    m_expLO[obsName] = CumDist(xvals.size(),{0,0});
+    m_expNLO[obsName] = CumDist(xvals.size(),{0,0});    
     p_resum->AddObservable(obsName,xvals);
+    
   }
 }
 
@@ -103,35 +122,36 @@ void NLL_Analysis::Evaluate(const ATOOLS::Blob_List& blobs,
   DEBUG_FUNC("");
   if(!p_resum) THROW(fatal_error, "Plugin not loaded.");
   if(!p_resum->Initialized()) return;
-  N += ncount;
-  sumW += weight;
-  sumW2 =+ sqr(weight);
+  m_Ncount += ncount;
+  m_sumW += weight;
+  m_sumW2 =+ sqr(weight);
   Particle_List* all = p_ana->GetParticleList(m_listname);
   if (all == nullptr) AddZero(ncount,0);
 
-  // find out channel
-  string channel = "";
-  for(Particle* p: *all) {
-    if(p->Flav().IsGluon()) channel = "g"+channel;
-    else channel += "q";
+  // find out channels
+  vector<string> channels;
+  for(const auto& alg: m_channelAlgs) {
+    channels.push_back(alg->Channel(*all));
   }
 
-  if(std::find(m_channels.begin(), m_channels.end(), channel) == m_channels.end()) {
-    // channel encountered for first time, initalize
-    m_channels.push_back(channel);
-    _h_NLL_channels[channel] = ObsDist();
-    _h_expLO_channels[channel] = ObsDist();
-    _h_expNLO_channels[channel] = ObsDist();
-    for(std::string& name: ObsNames) {
-      _h_NLL_channels[channel][name] = CumDist(_h_NLL[name].size(),{0,0});
-      _h_expLO_channels[channel][name] = CumDist(_h_expLO[name].size(),{0,0});
-      _h_expNLO_channels[channel][name] = CumDist(_h_expNLO[name].size(),{0,0});
+  for(const string& channel: channels) {
+    if(std::find(m_channels.begin(), m_channels.end(), channel)
+       == m_channels.end()) {
+      // channel encountered for first time, initalize
+      m_channels.push_back(channel);
+      m_NLL_channels[channel] = ObsDist();
+      m_expLO_channels[channel] = ObsDist();
+      m_expNLO_channels[channel] = ObsDist();
+      for(std::string& name: m_ObsNames) {
+        m_NLL_channels[channel][name] = CumDist(m_NLL[name].size(),{0,0});
+        m_expLO_channels[channel][name] = CumDist(m_expLO[name].size(),{0,0});
+        m_expNLO_channels[channel][name] = CumDist(m_expNLO[name].size(),{0,0});
+      }
     }
   }
-  
   // get results from resum an fill
-  for(size_t k=0; k<nObs; k++) {
-    const std::string& name = ObsNames[k];
+  for(size_t k=0; k<m_nObs; k++) {
+    const std::string& name = m_ObsNames[k];
     const std::vector<double>& resNLL = p_resum->m_resNLL[k];
     const std::vector<double>& resExpLO = p_resum->m_resExpLO[k];
     const std::vector<double>& resExpNLO = p_resum->m_resExpNLO[k];
@@ -140,42 +160,45 @@ void NLL_Analysis::Evaluate(const ATOOLS::Blob_List& blobs,
       const double nll = weight*resNLL[i];
       const double expLO = weight*resExpLO[i];
       const double expNLO = weight*resExpNLO[i];
-      _h_NLL[name][i].first += nll;
-      _h_NLL[name][i].second += sqr(nll);
-      _h_NLL_channels[channel][name][i].first += nll;
-      _h_NLL_channels[channel][name][i].second += sqr(nll);
-      _h_expLO[name][i].first += expLO;
-      _h_expLO[name][i].second += sqr(expLO);
-      _h_expLO_channels[channel][name][i].first += expLO;
-      _h_expLO_channels[channel][name][i].second += sqr(expLO);
-      _h_expNLO[name][i].first += expNLO;
-      _h_expNLO[name][i].second += sqr(expNLO);
-      _h_expNLO_channels[channel][name][i].first += expNLO;
-      _h_expNLO_channels[channel][name][i].second += sqr(expNLO);
+      m_NLL[name][i].first += nll;
+      m_NLL[name][i].second += sqr(nll);
+      m_expLO[name][i].first += expLO;
+      m_expLO[name][i].second += sqr(expLO);
+      m_expNLO[name][i].first += expNLO;
+      m_expNLO[name][i].second += sqr(expNLO);
+
+      for(const string& channel: channels) {
+        m_NLL_channels[channel][name][i].first += nll;
+        m_NLL_channels[channel][name][i].second += sqr(nll);
+        m_expLO_channels[channel][name][i].first += expLO;
+        m_expLO_channels[channel][name][i].second += sqr(expLO);
+        m_expNLO_channels[channel][name][i].first += expNLO;
+        m_expNLO_channels[channel][name][i].second += sqr(expNLO);
+      }
     }
   }
 }
 
 
 void NLL_Analysis::EndEvaluation(double scale) {
-  for(size_t k=0; k<nObs; k++) {
-    const std::string& name = ObsNames[k];
+  for(size_t k=0; k<m_nObs; k++) {
+    const std::string& name = m_ObsNames[k];
     const size_t n = p_resum->m_xvals[k].size();
     for(size_t i=0; i<n; i++) {
-      _h_NLL[name][i].first /= N;
-      _h_NLL[name][i].second /= N;
-      _h_expLO[name][i].first /= N;
-      _h_expLO[name][i].second /= N;
-      _h_expNLO[name][i].first /= N;
-      _h_expNLO[name][i].second /= N;
+      m_NLL[name][i].first /= m_Ncount;
+      m_NLL[name][i].second /= m_Ncount;
+      m_expLO[name][i].first /= m_Ncount;
+      m_expLO[name][i].second /= m_Ncount;
+      m_expNLO[name][i].first /= m_Ncount;
+      m_expNLO[name][i].second /= m_Ncount;
 
       for(const std::string& ch: m_channels) {
-        _h_NLL_channels[ch][name][i].first /= N;
-        _h_NLL_channels[ch][name][i].second /= N;
-        _h_expLO_channels[ch][name][i].first /= N;
-        _h_expLO_channels[ch][name][i].second /= N;
-        _h_expNLO_channels[ch][name][i].first /= N;
-        _h_expNLO_channels[ch][name][i].second /= N;
+        m_NLL_channels[ch][name][i].first /= m_Ncount;
+        m_NLL_channels[ch][name][i].second /= m_Ncount;
+        m_expLO_channels[ch][name][i].first /= m_Ncount;
+        m_expLO_channels[ch][name][i].second /= m_Ncount;
+        m_expNLO_channels[ch][name][i].first /= m_Ncount;
+        m_expNLO_channels[ch][name][i].second /= m_Ncount;
       }
     }
   }  
@@ -189,15 +212,15 @@ void NLL_Analysis::Output(const std::string& name, const CumDist& dist,
   ofile.Open();
   ofile->precision(ToType<int>(rpa->gen.Variable("HISTOGRAM_OUTPUT_PRECISION")));
   *ofile<<"# v Sigma{sumW sumW2 err} barSigma{sumW sumW2 err} NumEntries\n";
-  const double sigma = dist[dist.size()-1].first/N;
-  const double sigma2 = dist[dist.size()-1].second/N;
+  const double sigma = dist[dist.size()-1].first;
+  const double sigma2 = dist[dist.size()-1].second;
   for(size_t i=0; i<xvals.size(); i++) {
     const double sig = dist[i].first;
     const double sig2 = dist[i].second;
-    const double err = sqrt(sig2-sqr(sig))/(N-1);
+    const double err = sqrt(sig2-sqr(sig))/(m_Ncount-1);
     const double Bsig = sigma - dist[i].first;
     const double Bsig2 = sigma2 - dist[i].second;
-    const double Berr = sqrt(sig2-sqr(sig) + sigma2-sqr(sigma))/(N-1);
+    const double Berr = sqrt(sig2-sqr(sig) + sigma2-sqr(sigma))/(m_Ncount-1);
     *ofile<<xvals[i]<<" "<<sig<<" "<<sig2<<" "<<err<<" ";
     *ofile<<Bsig<<" "<<Bsig2<<" "<<Berr<<"\n";
   }
@@ -207,18 +230,18 @@ void NLL_Analysis::Output(const std::string& pname) {
   MakeDir(pname+"/NLL");
   MakeDir(pname+"/LO");
   MakeDir(pname+"/NLO");
-  for(size_t k=0; k<nObs; k++) {
-    const std::string& name = ObsNames[k];
-    Output(pname+"/NLL/"+name+"_Sigma.dat",_h_NLL[name],p_resum->m_xvals[k]);
-    Output(pname+"/LO/"+name+"_Sigma.dat",_h_expLO[name],p_resum->m_xvals[k]);
-    Output(pname+"/NLO/"+name+"_Sigma.dat",_h_expNLO[name],p_resum->m_xvals[k]);
+  for(size_t k=0; k<m_nObs; k++) {
+    const std::string& name = m_ObsNames[k];
+    Output(pname+"/NLL/"+name+"_Sigma.dat",m_NLL[name],p_resum->m_xvals[k]);
+    Output(pname+"/LO/"+name+"_Sigma.dat",m_expLO[name],p_resum->m_xvals[k]);
+    Output(pname+"/NLO/"+name+"_Sigma.dat",m_expNLO[name],p_resum->m_xvals[k]);
     for(const std::string& ch: m_channels) {
       Output(pname+"/NLL/Channel_"+ch+"_"+name+"_Sigma.dat",
-             _h_NLL_channels[ch][name], p_resum->m_xvals[k]);
+             m_NLL_channels[ch][name], p_resum->m_xvals[k]);
       Output(pname+"/LO/Channel_"+ch+"_"+name+"_Sigma.dat",
-             _h_expLO_channels[ch][name], p_resum->m_xvals[k]);
+             m_expLO_channels[ch][name], p_resum->m_xvals[k]);
       Output(pname+"/NLO/Channel_"+ch+"_"+name+"_Sigma.dat",
-             _h_expNLO_channels[ch][name], p_resum->m_xvals[k]);
+             m_expNLO_channels[ch][name], p_resum->m_xvals[k]);
     }
   }  
 }
