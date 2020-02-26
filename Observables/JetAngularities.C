9abd831ea507349e84db934e88663ecb4619f8d5
Vincent Theeuwes <vincent.theeuwes@uni-goettingen.de>
2020-02-26 23:09:24 +0100

    Fixed sign etamin

--- Observables/JetAngularities.C
+++ Observables/JetAngularities.C
@@ -42,7 +42,7 @@ namespace RESUM {
       const double a = 1;
       const double b = m_alpha-1;
       const double d = pow(2.*cosh(p[l].Eta())/(m_R),m_alpha-1) * (p[0]+p[1]).Abs()/(p[l].PPerp()*m_R);
-      const double etamin = -log(2.*cosh(p[l].Eta())/m_R);
+      const double etamin = log(2.*cosh(p[l].Eta())/m_R);
       return Obs_Params(a,b,log(d),0.0,etamin);
     }
 
