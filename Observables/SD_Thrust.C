247f4e307985dbc861edb19b14e002ffbfe07ecd
Vincent Theeuwes <vincent.theeuwes@uni-goettingen.de>
2020-09-03 16:42:47 +0200

    Fixed mistake m_ymax

--- Observables/SD_Thrust.C
+++ Observables/SD_Thrust.C
@@ -151,7 +151,7 @@ namespace RESUM {
       std::vector<Vec3D> vectors(n-nin);
       std::vector<Vec4D> momenta_ymax;
       for (size_t i(nin);i<n;++i){
-          if(0.<m_ymax && abs(rapidity(momenta[i]))<m_ymax){
+          if(m_ymax<0. || abs(rapidity(momenta[i]))<m_ymax){
             vectors[i-nin]=Vec3D(momenta[i][1],momenta[i][2],0.0);
             momenta_ymax.push_back(momenta[i]);
           }
